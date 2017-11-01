f00bffb0a63e0fabbf3ab132bebd4caaede945ff
Sajjad Hussain <sajjadhussainluqman@hotmail.com>
2017-11-02 01:01:51 +0500

    Final Implemented Basic Task

--- CommerceBit/CommerceBit/CommerceBitForm.cs
+++ CommerceBit/CommerceBit/CommerceBitForm.cs
@@ -1,16 +1,7 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.IO;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Formatting;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CommerceBit
